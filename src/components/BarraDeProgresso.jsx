f10b4fa424d0228b1e1b5a3eff3e4163b7159abd
terencioprata <terencioprata@gmail.com>
2025-11-10 19:36:04 +0100

    noite do dia 10/11/2025

--- src/components/BarraDeProgresso.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/BarraDeProgresso.jsx
-export default function BarraDeProgresso({ value = 0, max = 100 }) {
-  const pct = Math.round((value / max) * 100);
-  return (
-    <div className="w-full bg-white/10 rounded h-3 overflow-hidden">
-      <div style={{ width: `${pct}%` }} className="h-3 bg-brandGreen transition-all" />
-    </div>
-  );
-}
